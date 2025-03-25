2059061238f366fa8872cd17eb8230f980753856
Sebastian Strobl <sebastian@strobl.net>
2025-03-25 12:11:44 +0000

    Add: risc transform
    Signed-off-by: Radu-Mihai Costache <radu.mihai.costache1@huawei.com>

--- verify/asm2boogie/src/riscv/transform.rs
+++ verify/asm2boogie/src/riscv/transform.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn extract_riscv_functions(
     parsed: Vec<RiscvInstruction>,
@@ -12,7 +12,7 @@ pub fn extract_riscv_functions(
 
     for instr in parsed {
         match &instr {
-            RiscvInstruction::Label(name) if !name.starts_with(".") => {
+            RiscvInstruction::Label(name) if name.chars().next().map_or(false, |c| c.is_alphabetic() || c == '_') => {
                 if let Some((prev_name, prev_instrs)) = current_function {
                     functions.push(RiscvFunction {
                         name: prev_name,
@@ -58,7 +58,6 @@ pub fn extract_riscv_functions(
         None => functions,
     }
 }
-
 pub fn remove_directives(function: &RiscvFunction) -> RiscvFunction {
     let new_instructions = function
         .instructions
@@ -73,98 +72,108 @@ pub fn remove_directives(function: &RiscvFunction) -> RiscvFunction {
     }
 }
 
-
 pub fn transform_labels(function: &RiscvFunction) -> RiscvFunction {
-    let mut label_map = HashMap::new();
     let mut label_defs = Vec::new();
-    let mut unique_label_counter = 1;
+    let mut label_indices: HashMap<String, Vec<usize>> = HashMap::new();
 
     for (index, instruction) in function.instructions.iter().enumerate() {
         if let RiscvInstruction::Label(name) = instruction {
             if name != &function.name {
-                let unique_name = format!("l{}", unique_label_counter);
-                unique_label_counter += 1;
-                label_map.insert(index, unique_name.clone());
-                label_defs.push((name.clone(), index));
+                label_defs.push((index, name.clone()));
+                label_indices
+                    .entry(name.clone())
+                    .or_insert(Vec::new())
+                    .push(index);
             }
         }
     }
 
-    let mut label_indices: HashMap<String, Vec<usize>> = HashMap::new();
-    for (name, index) in &label_defs {
-        label_indices
-            .entry(name.clone())
-            .or_insert(Vec::new())
-            .push(*index);
-    }
     for indices in label_indices.values_mut() {
         indices.sort();
     }
 
-    let resolve_label = |label: &str, i: usize| -> Option<String> {
-        if label.ends_with('f') {
-            let base = &label[0..label.len() - 1];
-            if let Some(indices) = label_indices.get(base) {
-                if let Some(&j) = indices.iter().find(|&&j| j > i) {
-                    return label_map.get(&j).cloned();
-                }
+    let mut used_positions = HashSet::new();
+    for (i, instruction) in function.instructions.iter().enumerate() {
+        let label = match instruction {
+            RiscvInstruction::Branch { label, .. } | RiscvInstruction::Jump { label, .. } => label,
+            _ => continue,
+        };
+        if label.ends_with('f') || label.ends_with('b') {
+            if let Some(j) = find_label_position(label, i, &label_indices) {
+                used_positions.insert(j);
             }
-        } else if label.ends_with('b') {
-            let base = &label[0..label.len() - 1];
-            if let Some(indices) = label_indices.get(base) {
-                if let Some(&j) = indices.iter().rev().find(|&&j| j < i) {
-                    return label_map.get(&j).cloned();
-                }
-            }
-        } else {
-            return Some(label.to_string());
         }
-        None
-    };
+    }
+
+    let mut used_label_positions: Vec<usize> = label_defs
+        .iter()
+        .filter(|(pos, _)| used_positions.contains(pos))
+        .map(|(pos, _)| *pos)
+        .collect();
+    used_label_positions.sort();
+
+    let mut position_to_new_label = HashMap::new();
+    for (n, &pos) in used_label_positions.iter().enumerate() {
+        position_to_new_label.insert(pos, format!("L{}", n + 1));
+    }
 
     let mut new_instructions = Vec::new();
-    for (index, instruction) in function.instructions.iter().enumerate() {
+    for (i, instruction) in function.instructions.iter().enumerate() {
         match instruction {
-            RiscvInstruction::Label(name) => {
-                if name == &function.name {
-                    new_instructions.push(instruction.clone());
-                } else if let Some(unique_name) = label_map.get(&index) {
-                    new_instructions.push(RiscvInstruction::Label(unique_name.clone()));
-                } else {
-                    new_instructions.push(instruction.clone());
+            RiscvInstruction::Label(_name) => {
+                 if used_positions.contains(&i) {
+                    let new_name = position_to_new_label[&i].clone();
+                    new_instructions.push(RiscvInstruction::Label(new_name));
                 }
             }
-            RiscvInstruction::Branch {
-                op,
-                rs1,
-                rs2,
-                label,
-            } => {
-                if let Some(resolved_label) = resolve_label(label, index) {
-                    new_instructions.push(RiscvInstruction::Branch {
-                        op: op.clone(),
-                        rs1: rs1.clone(),
-                        rs2: rs2.clone(),
-                        label: resolved_label,
-                    });
+            RiscvInstruction::Branch { op, rs1, rs2, label } => {
+                if label.ends_with('f') || label.ends_with('b') {
+                    if let Some(j) = find_label_position(label, i, &label_indices) {
+                        if let Some(new_label) = position_to_new_label.get(&j) {
+                            new_instructions.push(RiscvInstruction::Branch {
+                                op: op.clone(),
+                                rs1: rs1.clone(),
+                                rs2: rs2.clone(),
+                                label: new_label.clone(),
+                            });
+                        } else {
+                            new_instructions.push(RiscvInstruction::Unhandled(format!(
+                                "Unresolved label: {}",
+                                label
+                            )));
+                        }
+                    } else {
+                        new_instructions.push(RiscvInstruction::Unhandled(format!(
+                            "Unresolved label: {}",
+                            label
+                        )));
+                    }
                 } else {
-                    new_instructions.push(RiscvInstruction::Unhandled(format!(
-                        "Unresolved label: {}",
-                        label
-                    )));
+                    new_instructions.push(instruction.clone());
                 }
             }
             RiscvInstruction::Jump { rd, label } => {
-                if let Some(resolved_label) = resolve_label(label, index) {
-                    new_instructions.push(RiscvInstruction::Jump {
-                        rd: rd.clone(),
-                        label: resolved_label,
-                    });
+                if label.ends_with('f') || label.ends_with('b') {
+                    if let Some(j) = find_label_position(label, i, &label_indices) {
+                        if let Some(new_label) = position_to_new_label.get(&j) {
+                            new_instructions.push(RiscvInstruction::Jump {
+                                rd: rd.clone(),
+                                label: new_label.clone(),
+                            });
+                        } else {
+                            new_instructions.push(RiscvInstruction::Unhandled(format!(
+                                "Unresolved label: {}",
+                                label
+                            )));
+                        }
+                    } else {
+                        new_instructions.push(RiscvInstruction::Unhandled(format!(
+                            "Unresolved label: {}",
+                            label
+                        )));
+                    }
                 } else {
-                    new_instructions.push(RiscvInstruction::Unhandled(format!(
-                        "Unresolved label: {}",
-                        label
-                    )));
+                    new_instructions.push(instruction.clone());
                 }
             }
             _ => new_instructions.push(instruction.clone()),
@@ -174,5 +183,25 @@ pub fn transform_labels(function: &RiscvFunction) -> RiscvFunction {
     RiscvFunction {
         name: function.name.clone(),
         instructions: new_instructions,
+    }
+}
+
+fn find_label_position(
+    label: &str,
+    i: usize,
+    label_indices: &HashMap<String, Vec<usize>>,
+) -> Option<usize> {
+    if label.ends_with('f') {
+        let base = &label[0..label.len() - 1];
+        label_indices
+            .get(base)
+            .and_then(|indices| indices.iter().find(|&&j| j > i).copied())
+    } else if label.ends_with('b') {
+        let base = &label[0..label.len() - 1];
+        label_indices
+            .get(base)
+            .and_then(|indices| indices.iter().rev().find(|&&j| j < i).copied())
+    } else {
+        None 
     }
 }
